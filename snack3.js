8ff5b4e43cbcace4376fbb4f51d254e78bde5d25
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-03 16:23:36 +0100

    FIX snack3 renaming

---
+++ snack3.js
@@ -0,0 +1,61 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: "Alice",
+      age: 35,
+    },
+    available: false,
+    price: "101€",
+    tags: ["advanced", "js", "react", "senior"],
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: "Bob",
+      age: 20,
+    },
+    available: true,
+    price: "25€",
+    tags: ["advanced", "js", "mid-senior"],
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: "Alice",
+      age: 17,
+    },
+    available: true,
+    price: "8€",
+    tags: ["html", "css", "junior"],
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: "Charlie",
+      age: 50,
+    },
+    available: false,
+    price: "48€",
+    tags: ["html", "advanced", "junior", "mid-senior"],
+  },
+];
+
+const authors = books.map((b) => b.author);
+
+function isAdult(e) {
+  return e.age >= 18;
+}
+
+const areAuthorsAdults = authors.every(isAdult);
+console.log(areAuthorsAdults);
+
+areAuthorsAdults
+  ? authors.sort((a, b) => a.age - b.age)
+  : authors.sort((a, b) => b.age - a.age);
+
+console.log(authors);
