45ee0e773a3c3f6ba62e6899acd158e96d4bf30e
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-03 16:28:00 +0100

    ADD snack4.js

---
+++ snack4.js
@@ -0,0 +1,55 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: "Alice",
+      age: 35,
+    },
+    available: false,
+    price: "101€",
+    tags: ["advanced", "js", "react", "senior"],
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: "Bob",
+      age: 20,
+    },
+    available: true,
+    price: "25€",
+    tags: ["advanced", "js", "mid-senior"],
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: "Alice",
+      age: 17,
+    },
+    available: true,
+    price: "8€",
+    tags: ["html", "css", "junior"],
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: "Charlie",
+      age: 50,
+    },
+    available: false,
+    price: "48€",
+    tags: ["html", "advanced", "junior", "mid-senior"],
+  },
+];
+
+const ages = books.map((e) => e.author.age);
+console.log(ages);
+
+const agesSum = ages.reduce((acc, curr) => {
+  return acc + curr;
+});
+
+console.log("L'età media degli autori è", agesSum / ages.length, "anni");
