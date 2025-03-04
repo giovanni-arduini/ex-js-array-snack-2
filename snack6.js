06ec8ec58cfd6016886d5cba6a26f05234813761
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-04 16:52:41 +0100

    ADD snack6 solution

---
+++ snack6.js
@@ -0,0 +1,59 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: "Alice",
+      age: 35,
+    },
+    available: false,
+    price: "101€",
+    tags: ["advanced", "js", "react", "senior"],
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: "Bob",
+      age: 20,
+    },
+    available: true,
+    price: "25€",
+    tags: ["advanced", "js", "mid-senior"],
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: "Alice",
+      age: 17,
+    },
+    available: true,
+    price: "8€",
+    tags: ["html", "css", "junior"],
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: "Charlie",
+      age: 50,
+    },
+    available: false,
+    price: "48€",
+    tags: ["html", "advanced", "junior", "mid-senior"],
+  },
+];
+
+const areThereAvailableBooks = books.some((book) => book.available == true);
+console.log(areThereAvailableBooks);
+
+const booksByPrice = books.sort(
+  (a, b) => parseInt(a.price) - parseInt(b.price)
+);
+
+booksByPrice.sort((a, b) =>
+  a.available === b.available ? 0 : a.available ? -1 : 1
+);
+
+console.log(booksByPrice);
